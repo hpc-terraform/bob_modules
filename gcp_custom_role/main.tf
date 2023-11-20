3585cf772e04cfe3576b0d6707f87a95663d7f91
Bob Clapp <bob.clapp@gmail.com>
2023-11-20 20:16:05 +0000

    testing

--- gcp_custom_role/main.tf
+++ gcp_custom_role/main.tf
@@ -6,12 +6,6 @@ locals {
 
 module "gcp_custom_role" {
   source = "github.com/hpc-terraform/bob_modules//gcp_custom_role"
-  billing_account_id = var.billing_account_id
-  description        = local.description
-  display_name       = local.display_name
-  role_id     = var.role_id
-  title       = var.title
-  permissions = var.permissions
-  project     = var.project_id
+
 }
 
